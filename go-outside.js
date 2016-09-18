6441b0ec40366a0dd2ecdcaa5d310bda660e9635
Bowtree00 <bowtree00@gmail.com>
2016-09-18 04:26:59 +0000

    added go-outside.js

---
+++ go-outside.js
@@ -0,0 +1,26 @@
+// var raining = true;
+// var cold = false;
+
+// // if (raining) {
+// //   console.log("Don't forget your umbrella!");
+// // }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleves are fine.") ;
+// }
+
+// // console.log("Now you're ready to go outside!");
+
+var temp = 20;
+
+if (temp < 0) {
+  console.log("Make sure - scarf");
+} else if (temp < 15) {
+  console.log("not short sleeves");
+} else {
+  console.log("short sleves are ok");
+}
+
+console.log("done")
